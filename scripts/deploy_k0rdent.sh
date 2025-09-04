a5e615bfe5d69793f62232c770bc383069625cb6
Josef Hak <jhak@mirantis.com>
2025-09-04 15:00:32 +0200

    Add KIND_CLUSTER env var to k0rdent deploy/remove scripts

--- scripts/deploy_k0rdent.sh
+++ scripts/deploy_k0rdent.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ -z "${KIND_CLUSTER:-}" ]]; then
-    KIND_CLUSTER="k0rdent"
-fi
-
-if kind get clusters | grep "$KIND_CLUSTER"; then
-    echo "$KIND_CLUSTER kind cluster already exists"
+kind_cluster="${KIND_CLUSTER:-k0rdent}"
+if kind get clusters | grep "${kind_cluster}"; then
+    echo "${kind_cluster} kind cluster already exists"
 else
-    kind create cluster -n "$KIND_CLUSTER"
+    kind create cluster -n "${kind_cluster}"
 fi
 
-kind get kubeconfig -n "$KIND_CLUSTER" > "kcfg_k0rdent"
-chmod 0600 "kcfg_k0rdent" # set minimum attributes to kubeconfig (owner read/write)
+kind get kubeconfig -n "${kind_cluster}" > "kcfg_${kind_cluster}"
+chmod 0600 "kcfg_${kind_cluster}" # set minimum attributes to kubeconfig (owner read/write)
 
 if [[ ${DEBUG:-} == "true" ]]; then
   HELM_EXTRA_FLAGS="--debug"
@@ -35,9 +32,9 @@ else
 fi
 
 if kubectl get ns | grep "kcm-system"; then
-    TEST_MODE=k0rdent NAMESPACE=kcm-system ./scripts/wait_for_deployment.sh
+    TEST_MODE="${kind_cluster}" NAMESPACE=kcm-system ./scripts/wait_for_deployment.sh
 fi
 
 if kubectl get ns | grep "projectsveltos"; then
-    TEST_MODE=k0rdent NAMESPACE=projectsveltos ./scripts/wait_for_deployment.sh
+    TEST_MODE="${kind_cluster}" NAMESPACE=projectsveltos ./scripts/wait_for_deployment.sh
 fi
