c87b8d85f958ed8925c6587abccd8a301a3b4cd8
Josef Hak <jhak@mirantis.com>
2025-10-08 11:41:28 +0200

    remove_mcs.sh - improve script

--- scripts/remove_mcs.sh
+++ scripts/remove_mcs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-kubectl delete multiclusterservice $APP -n kcm-system
+kubectl delete mcs $APP --wait=false
 
 ns=$(./scripts/get_mcs_namespace.sh)
 NAMESPACE=$ns ./scripts/wait_for_deployment_removal.sh
+kubectl delete multiclusterservice $APP 2>/dev/null
+
+if kubectl get mcs "$APP" 2>/dev/null; then
+    echo "❌ Multicluster service '$APP' not removed!"
+    exit 1
+fi
+echo "✅ Multicluster service '$APP' removed!"
