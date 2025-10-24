1e885c0e7f6a2fcece898da0ed9240ef5551abe2
Josef Hak <jhak@mirantis.com>
2025-10-24 15:16:49 +0200

    Support multi-cluster TEST_MODE for deploy_mcs.sh and remove_mcs.sh

--- scripts/deploy_mcs.sh
+++ scripts/deploy_mcs.sh
@@ -9,4 +9,22 @@ wfd=$(python3 ./scripts/utils.py get-wait-for-pods $APP)
 wfr=$(python3 ./scripts/utils.py get-wait-for-running $APP)
 ns=$(./scripts/get_mcs_namespace.sh)
 
-WAIT_FOR_PODS=$wfd WAIT_FOR_RUNNING=$wfr NAMESPACE=$ns ./scripts/wait_for_deployment.sh
+check_clusters() {
+  for test_mode in ${TEST_MODE//,/ }; do
+    (
+        export WAIT_FOR_PODS=$wfd
+        export WAIT_FOR_RUNNING=$wfr
+        export NAMESPACE=$ns
+        export TEST_MODE=$test_mode
+        ./scripts/wait_for_deployment.sh
+    )
+  done
+}
+
+check_clusters
+
+# for multi-cluster mode check it again in the end for better output readability
+if [[ "$TEST_MODE" == *,* ]]; then
+  echo -e "\nLet's check all clusters ($TEST_MODE) once again:"
+  check_clusters
+fi
