89ff222cf1dddeee736b86c82dbcd0c208958312
Josef Hak <jhak@mirantis.com>
2025-10-24 16:12:59 +0200

    scripts/install_servicetemplates.sh: use || true

--- scripts/install_servicetemplates.sh
+++ scripts/install_servicetemplates.sh
@@ -4,12 +4,11 @@ set -euo pipefail
 echo "Checking already installed"
 if helm list -n kcm-system | grep "$APP"; then
     echo "✅ App '$APP' service template already installed."
-    exit 0
+else
+    echo "Not installed yet"
+    echo "Installing k0rdent service templates based on apps/$APP/example/Chart.yaml deps"
+    install_st_commands=$(python3 ./scripts/utils.py install-servicetemplates $APP)
+    bash -c "$install_st_commands" || true
 fi
-echo "Not installed yet"
-
-echo "Installing k0rdent service templates based on apps/$APP/example/Chart.yaml deps"
-install_st_commands=$(python3 ./scripts/utils.py install-servicetemplates $APP)
-bash -c "$install_st_commands"
 
 ./scripts/wait_for_servicetemplate.sh
