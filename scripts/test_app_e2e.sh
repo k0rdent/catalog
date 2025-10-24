cb3448ba458f8c7d5ee73737096eff1efec85dc7
Josef Hak <jhak@mirantis.com>
2025-10-24 18:01:43 +0200

    Update arm validation data (data.yaml)

--- scripts/test_app_e2e.sh
+++ scripts/test_app_e2e.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Batch 1: ./scripts/test_app_e2e.sh ingress-nginx dex kyverno velero cert-manager external-secrets
+# Batch 2: ./scripts/test_app_e2e.sh tika arangodb argo-cd cadvisor ceph clearml cloudcasa cluster-autoscaler
+
+
 set -euo pipefail
 
 if [ $# -eq 0 ]; then
