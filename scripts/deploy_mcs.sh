9351980cbb174f0594a928c63a7378dd4ba37cd9
Josef Hak <jhak@mirantis.com>
2025-10-29 09:43:14 +0100

    Add "kubectl top nodes" to deploy_mcs.sh and remove_mcs.sh scripts

--- scripts/deploy_mcs.sh
+++ scripts/deploy_mcs.sh
@@ -10,3 +10,5 @@ wfr=$(python3 ./scripts/utils.py get-wait-for-running $APP)
 ns=$(./scripts/get_mcs_namespace.sh)
 
 WAIT_FOR_PODS=$wfd WAIT_FOR_RUNNING=$wfr NAMESPACE=$ns ./scripts/wait_for_deployment.sh
+
+KUBECONFIG="kcfg_$TEST_MODE" kubectl top nodes
