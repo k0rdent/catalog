3cfc3dee4d4bb8ee79ac95720248f6da4f01c0a0
Josef Hak <jhak@mirantis.com>
2026-05-26 10:58:21 +0200

    Remove old mkdocs deps
    - #1049

--- mkdocs/js/tablesort.js
+++
@@ -1,6 +0,0 @@
-document$.subscribe(function() {
-  var tables = document.querySelectorAll("article table:not([data-sort='false'])")
-  tables.forEach(function(table) {
-    new Tablesort(table)
-  })
-})
